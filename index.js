0cb37ef431708709af5964d74523f9fd69808c14
Colin Schlecht <schlechtcolin@yahoo.com>
2021-02-28 18:55:18 -0800

    commit tree 60e7e632ccfef75c4e088a1de9308246fa1057b6

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
+
+// //Standard Function Declaration
+// function addSomething(something){
+//   return something + something;
+// };
+
+// //Standard function expression
+// const addition = function addSomething(something) { 
+//   return something + something;
+// };
+
+// // Same Function (but anonymous)
+// const addition = function(something) { 
+//   return something + something;
+// };
+
+// // Anonymous Arrow Function
+// const addition = (something) => { 
+//   return something + something;
+// };
+
+// // Remove brackets, return, parenthesis
+// const addition = something => something + somehting;
+
+
+// function addingEventListener() {
+//   const input = document.getElementById('input');
+// input.addEventListener('click', function(event) {
+// alert('I was clicked!');
+// });
+// }
+// addingEventListener() 
+
 function addingEventListener() {
+  const input = document.getElementById('input');
+
+
+input.addEventListener('click', () => {
+  alert('I was clicked!');
+});
 }
+addingEventListener() 
+
+
+
